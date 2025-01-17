107d77dfd0e52c75b4bdb57be4839c5cc8c28a0e
Sandip Sarraf <Sandip.Sarraf@Student.HTW-Berlin.de>
2025-01-17 02:49:53 +0100

    Fixed resume

--- components/nav/Heading.tsx
+++ components/nav/Heading.tsx
@@ -6,7 +6,7 @@ export const Heading = () => {
   return (
     <header className={styles.heading}>
       <MyLinks />
-      <OutlineButton onClick={() => window.open("/CV.pdf")}>
+      <OutlineButton onClick={() => window.open("/Resume.pdf")}>
         My resume
       </OutlineButton>
     </header>
